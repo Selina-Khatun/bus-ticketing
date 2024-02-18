bab16c19c0093f404e7ab6fd3bca3daaf0dc2ef7
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2024-02-19 02:15:34 +0600

    added paribahan section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+ 
   theme: {
+    fontFamily: {
+      'sans': ['Inter', 'sans-serif'],
+      'serif': ['Raleway', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
