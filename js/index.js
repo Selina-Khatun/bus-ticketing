db53df42d2c42e6f8ffa99e623b6727b1f014930
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2024-02-19 06:12:40 +0600

    added grand total calculation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 const ticketButtons = document.querySelectorAll(".ticket");
 const totalPriceDisplay = document.getElementById("totalPrice");
-const totalTicketsDisplay = document.getElementById('selectedItem').querySelector('span');
+const totalTicketsDisplay = document
+  .getElementById("selectedItem")
+  .querySelector("span");
+  const grandTotalDisplay = document.getElementById("grandTotal");
+const couponCodeInput = document.getElementById("coupon-code");
+const applyButton = document.getElementById("apply-button");
+
 let selectedTickets = [];
+
 ticketButtons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.classList.contains("selected")) {
@@ -20,28 +27,73 @@ ticketButtons.forEach((button) => {
     updateTable();
   });
 });
+
 function updateTable() {
-    const tbody = document.querySelector('#ticketTable tbody ');
-    const thead=document.getElementById('selectedItem')
-    tbody.innerHTML = '';
-    thead.innerText=selectedTickets.length;
-    selectedTickets.forEach(ticket => {
-        const tr = document.createElement('tr');
-        const tdTicket = document.createElement('td');
-        tdTicket.textContent = ticket;
-        tr.appendChild(tdTicket);
+  const tbody = document.querySelector("#ticketTable tbody ");
+  const thead = document.getElementById("selectedItem");
+  tbody.innerHTML = "";
+  thead.innerText = selectedTickets.length;
 
-        const tdType = document.createElement('td');
-        tdType.textContent = 'Economy'; 
-        tr.appendChild(tdType);
+  const nextButton = document.getElementById("nextButton");
+  nextButton.disabled = selectedTickets.length < 1;
 
-        const tdPrice = document.createElement('td');
-        tdPrice.textContent = '550'; 
-        tr.appendChild(tdPrice);
+ 
+  selectedTickets.forEach((ticket) => {
+    const tr = document.createElement("tr");
+    const tdTicket = document.createElement("td");
+    tdTicket.textContent = ticket;
+    tr.appendChild(tdTicket);
 
-        tbody.appendChild(tr);
-    });
+    const tdType = document.createElement("td");
+    tdType.textContent = "Economy";
+    tr.appendChild(tdType);
 
-    totalPriceDisplay.textContent = 'BDT  ' + (selectedTickets.length * 550);
-    
+    const tdPrice = document.createElement("td");
+    tdPrice.textContent = "550";
+    tr.appendChild(tdPrice);
+
+    tbody.appendChild(tr);
+  });
+
+   const totalAmount = selectedTickets.length * 550; 
+  totalPriceDisplay.textContent = "BDT " + totalAmount.toFixed(2);
+
+// apply button calculation starts from here
+
+applyButton.disabled = selectedTickets.length < 4;
+
+applyButton.addEventListener("click", function () {
+  if (!applyButton.disabled && couponCodeInput.value === "NEW15") {
+    const discount = 0.15;
+    let totalAmount=selectedTickets.length * 550;
+    console.log(totalAmount)
+    let discountedTotal = totalAmount * discount;
+    console.log('discountedTotal', discountedTotal);
+    let total = totalAmount - discountedTotal;
+    console.log('total',total)
+    grandTotalDisplay.textContent = "BDT " + total.toFixed(2);
+    couponCodeInput.value = ""; 
+}
+    else if(!applyButton.disabled && couponCodeInput.value === "Couple 20"){
+      const discount = 0.20;
+      let totalAmount=selectedTickets.length * 550;
+      console.log(totalAmount)
+      let discountedTotal = totalAmount * discount;
+      console.log('discountedTotal', discountedTotal);
+      let total = totalAmount - discountedTotal;
+      console.log('total',total)
+      grandTotalDisplay.textContent = "BDT " + total.toFixed(2);
+      couponCodeInput.value = "";  
+    }
+  
+});
+
+grandTotalDisplay.textContent = "BDT " + totalAmount.toFixed(2);
+}
+function Continue() {
+
+    window.Location.href = "http://127.0.0.1:5500/index.html";
+
+    window.location.reload()
+
 }
